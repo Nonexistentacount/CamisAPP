c5ffe4306665e803b4856b4098249985d5d313ac
Ariadne Balmaceda <2022014@est.cedesdonbosco.ed.cr>
2026-06-15 16:17:25 -0600

    Cambios para el index

--- index.js
+++ index.js
@@ -81,3 +81,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor API escuchando en http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
